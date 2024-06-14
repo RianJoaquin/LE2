53ebd7a9aefe4b21b1392baaa4a8a0637f8214fa
RianJoaquin <rggjoaquin@mymail.mapua.edu.ph>
2024-06-14 17:32:53 +0800

    Add files via upload

---
+++ JOAQUINLE2/Data/SqlData.cs
@@ -0,0 +1,17 @@
+﻿using BlogDataLibrary.Database;
+using System;
+using System.Collections.Generic;
+
+namespace BlogDataLibrary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDB";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
